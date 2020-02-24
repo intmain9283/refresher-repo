8daa7e31befa544bf2355562e697049752408647
intmain9283 <spartanpride83@yahoo.com>
2020-02-23 23:09:56 -0500

    Added comments to functions
    Added comments that better describe what the functions do

--- Counting.java
+++ Counting.java
@@ -1,22 +1,27 @@
 public class Counting{
+
+	//Function to count to 10
 	public void CountTo10() {
 		for (int i = 0; i < 10; i++) {
 			System.out.printf("Number: %d", i+1);
 		}
 	}
 	
+	//Function to count to 100
 	public void CountTo100() {
 		for (int i = 0; i < 100; i++) {
 			System.out.printf("Number %d", i+1);
 		}
 	}
 	
+	//Function to count by 5
 	public CountBy5() {
 		for (int i = 0; i < 100; i=+5) {
 			System.out.printf("Number: %d", i);
 		}
 	}
 
+	//Main method
 	public static void main (String[] args) {
 		for (int i = 0; i < 1000; i=+10) {
 			System.out.printf("Number: %d", i);
