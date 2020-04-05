dbb64c19ebad251e06dd000c663e61c6eea9ea45
intmain9283 <spartanpride83@yahoo.com>
2020-04-04 23:29:27 -0400

    Added variables for newly declared function introduction(); added comments for readability and called introduction() for testing.

--- pythonPractice.py
+++ pythonPractice.py
@@ -1,6 +1,21 @@
 import os
 
+#Hello World statements
 print("Hello, world")
 print("Hello, nurse")
 
+#Variable declarations
+age = 27
+gamer = True
+favGame = "XCOM 2"
+
+#Function declarations
+def introduction(age1, gamer1, favGame1):
+	str = "Hi. I am {} years old."
+	print(str.format(age1))
+	
+	if gamer1 == True:
+		print("I am a gamer and my favorite game is " + favGame1)
+		
+introduction(age, gamer, favGame)
 os.system("pause")
